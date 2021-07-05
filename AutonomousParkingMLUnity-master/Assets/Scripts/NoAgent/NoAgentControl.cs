b31410c5540ef57a813b6aa4d95e824effadd067
unknown <aa16001@essex.ac.uk>
2021-07-05 01:55:13 +0100

    project

---
+++ AutonomousParkingMLUnity-master/Assets/Scripts/NoAgent/NoAgentControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NoAgentControl : MonoBehaviour
+{
+
+
+    public GameObject endPosition;
+    private NavMeshAgent navAgent;
+    private AutoParkAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = this.GetComponent<NavMeshAgent>();
+        agent = this.GetComponent<AutoParkAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        endPosition = GameObject.FindGameObjectWithTag("Finish");
+        navAgent.SetDestination(new Vector3(-2.4f,0, endPosition.transform.position.z+2 ));
+
+        //     agent.SetModel();
+    }
+}
